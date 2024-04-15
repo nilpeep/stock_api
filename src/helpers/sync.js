1ff93848c74406f7ee7d67ddcf95637d235705ed
nilpeep <niluferk038@gmail.com>
2024-04-15 21:42:35 +0300

    token controller

--- src/helpers/sync.js
+++ src/helpers/sync.js
@@ -1,7 +1,5 @@
 "use strict"
-/* -------------------------------------------------------
-    NODEJS EXPRESS | CLARUSWAY FullStack Team
-------------------------------------------------------- */
+
 // sync():
 
 module.exports = async function () {
