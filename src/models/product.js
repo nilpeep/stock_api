86b2122e491b8b9ea555139b133761b1a586ea29
nilpeep <niluferk038@gmail.com>
2024-04-17 21:49:40 +0300

    purchase model

--- src/models/product.js
+++ src/models/product.js
@@ -20,6 +20,10 @@ const ProductSchema = new mongoose.Schema({
         required: true,
         trim: true
     },
+    price:{
+        type: Number,
+        required: true
+    },
     quantity:{
         type: Number,
         default: 0
