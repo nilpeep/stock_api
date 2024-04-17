048456e744a764373fd28e0199f68e1c960cdbab
nilpeep <niluferk038@gmail.com>
2024-04-17 21:24:18 +0300

    brand router

--- src/routes/brand.js
+++ src/routes/brand.js
@@ -1,6 +1,22 @@
 "use strict"
-/* -------------------------------------------------------
-    NODEJS EXPRESS | CLARUSWAY FullStack Team
-------------------------------------------------------- */
+
 const router = require('express').Router()
-/* ------------------------------------------------------- */
+
+//? brand router:
+
+const brand = require('../controllers/brand')
+const permissions = require('../middlewares/permissions')
+
+// URL: /brands
+
+router.route('/')
+.get(permissions.isStaff, brand.read)
+.post(permissions.isAdmin, brand.create)
+
+router.route('/:id')
+.get(permissions.isStaff, brand.read)
+.put(permissions.isAdmin, brand.update)
+.patch(permissions.isAdmin, brand.update)
+.delete(permissions.isAdmin, brand.delete)
+
+module.exports = router
