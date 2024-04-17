04278e5f3c1131fda4382b90916fddca27eaa339
nilpeep <niluferk038@gmail.com>
2024-04-17 21:30:36 +0300

    firm router

--- src/routes/firm.js
+++ src/routes/firm.js
@@ -1,6 +1,22 @@
 "use strict"
-/* -------------------------------------------------------
-    NODEJS EXPRESS | CLARUSWAY FullStack Team
-------------------------------------------------------- */
+
 const router = require('express').Router()
-/* ------------------------------------------------------- */
+
+//? firm router:
+
+const firm = require('../controllers/firm')
+const permissions = require('../middlewares/permissions')
+
+// URL: /firms
+
+router.route('/')
+.get(permissions.isStaff, firm.read)
+.post(permissions.isAdmin, firm.create)
+
+router.route('/:id')
+.get(permissions.isStaff, firm.read)
+.put(permissions.isAdmin, firm.update)
+.patch(permissions.isAdmin, firm.update)
+.delete(permissions.isAdmin, firm.delete)
+
+module.exports = router
