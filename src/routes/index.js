84a85b5515974d6a2aa0e627af16ab9bd2c46ca6
nilpeep <niluferk038@gmail.com>
2024-04-17 21:53:34 +0300

    sales route

--- src/routes/index.js
+++ src/routes/index.js
@@ -26,8 +26,8 @@ router.use('/firms', require('./firm'))
 router.use('/products', require('./product'))
 // purchase:
 router.use('/purchases', require('./purchase'))
-// // sale:
-// router.use('/sales', require('./sale'))
+// sale:
+router.use('/sales', require('./sale'))
 
 // document:
 router.use('/documents', require('./document'))
--- src/routes/sale.js
+++ src/routes/sale.js
@@ -1,6 +1,22 @@
 "use strict"
-/* -------------------------------------------------------
-    NODEJS EXPRESS | CLARUSWAY FullStack Team
-------------------------------------------------------- */
+
 const router = require('express').Router()
-/* ------------------------------------------------------- */
+
+//? sale router:
+
+const sale = require('../controllers/sale')
+const permissions = require('../middlewares/permissions')
+
+// URL: /sales
+
+router.route('/')
+.get(permissions.isStaff, sale.read)
+.post(permissions.isAdmin, sale.create)
+
+router.route('/:id')
+.get(permissions.isStaff, sale.read)
+.put(permissions.isAdmin, sale.update)
+.patch(permissions.isAdmin, sale.update)
+.delete(permissions.isAdmin, sale.delete)
+
+module.exports = router
