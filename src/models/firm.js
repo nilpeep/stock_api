fcbc3662793fa7c2180dd0bbd23c2c303420accf
nilpeep <niluferk038@gmail.com>
2024-04-17 21:29:06 +0300

    firm model

--- src/models/firm.js
+++ src/models/firm.js
@@ -1,6 +1,30 @@
-"use strict"
-/* -------------------------------------------------------
-    NODEJS EXPRESS | CLARUSWAY FullStack Team
-------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
-/* ------------------------------------------------------- */
+"use strict";
+
+const { mongoose } = require("../configs/dbConnection");
+
+//? Firm model:
+
+const FirmSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+
+    phone: String,
+
+    address: String,
+
+    email: String,
+
+    image: String,
+  },
+  {
+    collection: "firms",
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Firm", FirmSchema);
