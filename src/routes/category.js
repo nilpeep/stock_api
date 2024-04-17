1a56e235d48ef54afbc704dba90dd8d8f19fcdf7
nilpeep <niluferk038@gmail.com>
2024-04-17 20:49:35 +0300

    category router

--- src/routes/category.js
+++ src/routes/category.js
@@ -1,6 +1,22 @@
 "use strict"
-/* -------------------------------------------------------
-    NODEJS EXPRESS | CLARUSWAY FullStack Team
-------------------------------------------------------- */
+
 const router = require('express').Router()
-/* ------------------------------------------------------- */
+
+//? Category router:
+
+const category = require('../controllers/category')
+const permissions = require('../controllers/permissions')
+
+// URL: /categories
+
+router.route('/')
+.get(permissions.isStaff, category.list)
+.post(permissions.isAdmin, category.create)
+
+router.route('/:id')
+.get(permissions.isStaff, category.read)
+.put(permissions.isAdmin, category.update)
+.patch(permissions.isAdmin, category.update)
+.delete(permissions.isAdmin, category.delete)
+
+module.exports = router
