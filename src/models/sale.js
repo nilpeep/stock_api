c4f83ddf4f9bc8c8455ef5e42d21735e16b97802
nilpeep <niluferk038@gmail.com>
2024-04-17 21:52:24 +0300

    sale model

--- src/models/sale.js
+++ src/models/sale.js
@@ -1,6 +1,44 @@
 "use strict"
-/* -------------------------------------------------------
-    NODEJS EXPRESS | CLARUSWAY FullStack Team
-------------------------------------------------------- */
+
 const { mongoose } = require('../configs/dbConnection')
-/* ------------------------------------------------------- */
+
+//? Sale model:
+
+const SaleSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    brandId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Brand',
+        required: true
+    },
+    productId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    amount: {
+        type: Number,
+        default:1
+    },
+    total: {
+        type:Number,
+        default:function(){return this.price * this.amount}, // if value did not sent
+        transform:function(){return this.price * this.amount}, // if value updated
+        set:function(){return this.price * this.amount} // if value sent
+    }
+},{
+    timestamps: true,
+    collection: 'Sales'
+})
+
+//? exports:
+
+module.exports = mongoose.model('Sale', SaleSchema)
